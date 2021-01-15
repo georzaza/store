39b4453b5e5df5c1ea31db56ef46d69dd1bfb4f7
Giorgos Zazanis <georzaza@gmail.com>
2021-01-15 21:28:22 +0200

    another one

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -9,67 +9,44 @@
   </div>   
 </div>
 
-<div class="row">
-  	<div class="col-sm-12" >
-		<h1 class="display-4" style="text-align:center;">Recipes</h1>
-		
-		<form class="form-inline" style="margin-left:80%">
-			<input type="text"  size="15"  id="insearch" name="insearch" placeholder="Search for recipe" >
-    		<button type="submit" class="btn btn-primary">Go</button>
-		</form>
-		<br>
+<div>
+	<h1 style="text-align:center;">Recipes</h1>
+</div>
 
-		<a style="margin-bottom: 2%;" href="{{ route('recipes.create')}}" class="btn btn-primary">Add New Recipe</a>  
-    	<table class="table">
-      		<thead>
-	        	<tr>
-					<td>recipe_name </td>
-					<!--<td>execution </td>
-					<td>ingredient_name</td>
-					<td>Qty</td>-->
-          			<td colspan = 2>Actions</td>
-        		</tr>
-    		</thead>
-      		<tbody>
-        		@foreach($recipes as $item)
-        			<tr>
-						<td>{{$item->recipe_name }}</td>
-						<!--<td style="text-align:center;">{{$item->execution }}</td>
-						<td style="text-align:center;">{{$item->ingredient_name }}</td>
-						<td style="text-align:center;">{{$item->qty }}</td>-->
-						<td>
-	              			<a href="{{ route('recipes.edit',$item->recipe_id)}}" class="btn btn-primary">Edit Recipe</a>
-						</td>
-          				<td>
-		            		<form action="{{ route('recipes.destroy', $item->recipe_id)}}" method="post">
-              					@csrf
-              					@method('DELETE')
-	                				<button class="btn btn-danger" type="submit">Delete</button>
-              				</form>
-          				</td>
-        			</tr>
-        		@endforeach
-      		</tbody>
-    	</table>
-  	</div>
+<div class="row">
+
+	<div class="col-sm-12 ">
+		<form class="form-inline" style="margin-left:80%">
+			<input type="text"  size="15"  id="insearch" name="insearch" placeholder="Search for recipes" >
+    		<button type="submit" class="btn btn-primary">Go</button>
+		</form>	
+	</div>
+	<br>
+	<br>
+
+
+	<div class="col-sm-7" style="margin-left:10%;">
+		<a style="margin-bottom: 2%;" href="{{ route('recipes.create')}}" class="btn btn-primary">Add New Recipe</a>
+		<br>
+		<br> 
+		<caption>List of Recipes</caption>
+		<hr>
+		<br>
+		@foreach($recipes as $item)
+			<a href="{{ route('recipes.show',$item->recipe_id)}}" style="margin-right:5%;">{{$item->recipe_name }}</a>
+			<a href="{{ route('recipes.edit',$item->recipe_id)}}" style="margin-right:5%;" class="btn btn-primary">Edit</a>
+			<a href="{{ route('recipes.destroy',$item->recipe_id)}}"  class="btn btn-danger">Delete</a>
+		@endforeach
+	</div>
 </div>
 
 <div class="col-sm-12">
-  @if(session()->get('success'))
-    <div class="alert alert-success">
-      {{ session()->get('success') }}  
-    </div>
-  @endif
-
-<br>
-<form action="{{ route('CreateQuestion.store') }}" method="post">
-  <div class="form-group">
-    <label for="1stquestion" style="font-size:18px" >Search for recipes containing the following ingredient: </label>
 	<br>
-    <input type="text" size="20" id="ingredient" name="ingredient" placeholder="ingredient" >
-    <button type="submit" class="btn btn-primary">Search</button>
-  </div>
-</form>
+  	@if(session()->get('success'))
+		<div class="alert alert-success">
+	  		{{ session()->get('success') }} 
+    	</div>
+  	@endif
 </div>
 
 
--- storage/framework/views/9f381ad7c14183cda3f7d64bfc95fcc2f99c316f.php
+++ storage/framework/views/9f381ad7c14183cda3f7d64bfc95fcc2f99c316f.php
@@ -8,67 +8,56 @@
   </div>   
 </div>
 
-<div class="row">
-  	<div class="col-sm-12" >
-		<h1 class="display-4" style="text-align:center;">Recipes</h1>
-		
-		<form class="form-inline" style="margin-left:80%">
-			<input type="text"  size="15"  id="insearch" name="insearch" placeholder="Search for recipe" >
-    		<button type="submit" class="btn btn-primary">Go</button>
-		</form>
-		<br>
+<div>
+	<h1 style="text-align:center;">Recipes</h1>
+</div>
 
-		<a style="margin-bottom: 2%;" href="<?php echo e(route('recipes.create')); ?>" class="btn btn-primary">Add New Recipe</a>  
-    	<table class="table">
-      		<thead>
-	        	<tr>
-					<td>recipe_name </td>
-					<!--<td>execution </td>
-					<td>ingredient_name</td>
-					<td>Qty</td>-->
-          			<td colspan = 2>Actions</td>
-        		</tr>
-    		</thead>
+<div class="row">
+
+	<div class="col-sm-12 ">
+		<form class="form-inline" style="margin-left:80%">
+			<input type="text"  size="15"  id="insearch" name="insearch" placeholder="Search for recipes" >
+    		<button type="submit" class="btn btn-primary">Go</button>
+		</form>	
+	</div>
+	<br>
+	<br>
+
+
+	<div class="col-sm-7" style="margin-left:10%;">
+		<a style="margin-bottom: 2%;" href="<?php echo e(route('recipes.create')); ?>" class="btn btn-primary">Add New Recipe</a>
+		<br>
+		<br> 
+		<caption>List of Recipes</caption>
+		<hr>
+		<br>
+		<table class="table table-hover table-borderless" col>
       		<tbody>
         		<?php $__currentLoopData = $recipes; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
         			<tr>
 						<td><?php echo e($item->recipe_name); ?></td>
-						<!--<td style="text-align:center;"><?php echo e($item->execution); ?></td>
-						<td style="text-align:center;"><?php echo e($item->ingredient_name); ?></td>
-						<td style="text-align:center;"><?php echo e($item->qty); ?></td>-->
+						<!--<td ><?php echo e($item->execution); ?></td>
+						<td ><?php echo e($item->ingredient_name); ?></td>
+						<td ><?php echo e($item->qty); ?></td>-->
 						<td>
-	              			<a href="<?php echo e(route('recipes.edit',$item->recipe_id)); ?>" class="btn btn-primary">Edit Recipe</a>
+	              			<a href="<?php echo e(route('recipes.edit',$item->recipe_id)); ?>" class="btn btn-primary">Edit that shit</a>
 						</td>
           				<td>
-		            		<form action="<?php echo e(route('recipes.destroy', $item->recipe_id)); ?>" method="post">
-              					<?php echo csrf_field(); ?>
-              					<?php echo method_field('DELETE'); ?>
-	                				<button class="btn btn-danger" type="submit">Delete</button>
-              				</form>
+							<a href="<?php echo e(route('recipes.destroy',$item->recipe_id)); ?>" class="btn btn-danger">Delete</a>
           				</td>
         			</tr>
         		<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
       		</tbody>
-    	</table>
-  	</div>
+	</div>
 </div>
 
 <div class="col-sm-12">
-  <?php if(session()->get('success')): ?>
-    <div class="alert alert-success">
-      <?php echo e(session()->get('success')); ?>  
-    </div>
-  <?php endif; ?>
-
-<br>
-<form action="<?php echo e(route('CreateQuestion.store')); ?>" method="post">
-  <div class="form-group">
-    <label for="1stquestion" style="font-size:18px" >Search for recipes containing the following ingredient: </label>
 	<br>
-    <input type="text" size="20" id="ingredient" name="ingredient" placeholder="ingredient" >
-    <button type="submit" class="btn btn-primary">Search</button>
-  </div>
-</form>
+  	<?php if(session()->get('success')): ?>
+		<div class="alert alert-success">
+	  		<?php echo e(session()->get('success')); ?> 
+    	</div>
+  	<?php endif; ?>
 </div>
 
 
